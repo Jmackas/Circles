2e57fb27d4b4f6a22fbc94a55db1dc0581dc9c10
James Mackay <jamesrossmackay@gmail.com>
2017-12-28 15:42:33 +0800

    Complete function

--- scripts.js
+++ scripts.js
@@ -1,6 +1,27 @@
+// Total area function
+function areaOfCircle() {
 
-// Area of circle function
-function piArea() {
+    // Variable declarations
+    var radius;
+    var totalArea;
+
+    // Retrieve user input
+    radius = document.getElementById("inputRadius").value;
+    totalArea = document.getElementById("inputTotalArea").value;    
+
+    // Algorithm
+    totalArea = Math.PI * Math.pow(radius, 2);
+
+    // Round the answer to two decimal places
+    totalArea = totalArea.toFixed(2);
+
+    // Output to user
+    document.getElementById("inputTotalArea").value = totalArea;
+}
+
+// Radius function
+function radiusOfCircle() {
+
     // Variable declarations
     var radius;
     var totalArea;
@@ -9,29 +30,12 @@ function piArea() {
     radius = document.getElementById("inputRadius").value;
     totalArea = document.getElementById("inputTotalArea").value;
 
-    alert("inputTotalArea");
+    // Algorithm
+    radius = Math.sqrt(totalArea / Math.PI);
 
-    // Total area function
-    function areaOfCircle() {
-        totalArea == "";
-        totalArea = Math.PI * Math.pow(radius, 2);
-        
-        // Round the answer to two decimal places
-        totalArea = totalArea.toFixed(2);
+    // Round the answer to two decimal places
+    radius = radius.toFixed(2);
 
-        // Output to user
-        document.getElementById("inputTotalArea").value = totalArea;
-    }
-
-    // Radius function
-    function radiusOfCircle() {
-        radius == "";
-        radius = Math.sqrt(totalArea / Math.PI);
-
-        // Round the answer to two decimal places
-        radius = radius.toFixed(2);
-
-        // Output to user
-        document.getElementById("inputRadius").value = radius;
-    }
+    // Output to user
+    document.getElementById("inputRadius").value = radius;
 }
