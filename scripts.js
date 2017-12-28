76e1e6ccccc451b3a5ae77b84f17b416ddda7893
James Mackay <jamesrossmackay@gmail.com>
2017-12-28 14:16:13 +0800

    Update

--- scripts.js
+++ scripts.js
@@ -1,16 +1,18 @@
 
+
+
 // Area of circle function
 function areaOfCircle() {
-    
+
     // Variable declarations
     var radius;
     var finalAnswer;
-    
+
     // Retrieve user input
     radius = document.getElementById("inputRadius").value;
-    
+
     // Algorithm
-    finalAnswer = Math.PI * radius;
+    finalAnswer = Math.PI * Math.pow(radius, 2);
 
     // Output to user
     document.getElementById("outputCircleArea").innerHTML = finalAnswer;
